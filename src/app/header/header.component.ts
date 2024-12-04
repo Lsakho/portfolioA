6c69eb35bbbfa86baefa64088c5100b7cc769f89
Lsakho <sakho.lamine2k19@gmail.com>
2024-12-04 16:55:20 +0100

    adding header content

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
 
+
+export class HeaderComponent {
+  
+  constructor(private renderer: Renderer2, private el: ElementRef) {}
+
+  scrollToSection(section: string): void {
+    const element = this.el.nativeElement.querySelector(`#${section}`);
+    if (element) {
+      element.scrollIntoView({ behavior: 'smooth' });
+    }
+  }
 }
